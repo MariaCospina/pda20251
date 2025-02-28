f6311ac8798ff348f4d5ba967782856e1ff3c33b
MariaCospina <kristalblue140@hotmail.com>
2025-02-28 02:00:05 +0000

    guardar cambios

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ from setuptools import setup, find_packages
 setup(
     name="pad",
     version="0.0.1",
-    author="Andres Callejas",
+    author="Cristina ospina ",
     author_email="",
     description="",
-    py_modules=["actividad_1.py"],
+    py_modules=["actividad_1"],
     install_requires=[
         "pandas",
         "requests",
         "matplotlib",
-    ]
-    
-    
+        "os",
+    ],  
 )
